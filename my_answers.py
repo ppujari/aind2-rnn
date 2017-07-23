1883a7e6dbba245438c21c7de2583091be4e948c
Pradeep <ppujari@hotmail.com>
2017-07-23 14:57:10 -0700

    first commit

--- my_answers.py
+++ my_answers.py
@@ -8,39 +8,95 @@ import keras
 
 # TODO: fill out the function below that transforms the input series 
 # and window-size into a set of input/output pairs for use with our RNN model
-def window_transform_series(series, window_size):
+def window_transform_series(series,window_size):
     # containers for input/output pairs
     X = []
     y = []
-
+    size = series.shape[0]
+    start = 0
+    while (start < size - window_size):
+        a = []
+        b = []
+        count = 0 
+        while ( count < window_size ):
+            a.append(series[start + count])
+            count = count + 1
+        b.append(series[start + count])
+        start = start + 1
+        X.append(a)
+        y.append(b)
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
     y.shape = (len(y),1)
-
+    
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
-def build_part1_RNN(window_size):
-    pass
+def build_part1_RNN(step_size, window_size):
+### TODO: create required RNN model
+# import keras network libraries
+	from keras.models import Sequential
+	from keras.layers import Dense
+	from keras.layers import LSTM
+	import keras
+
+# given - fix random seed - so we can all reproduce the same results on our default time series
+	np.random.seed(0)
+
+# TODO: build an RNN to perform regression on our time series input/output data
+	model = Sequential()
+	model.add(LSTM(5, input_shape=(window_size, 1), activation='relu'))
+	model.add(Dense(1, activation='tanh'))
+
+# build model using keras documentation recommended optimizer initialization
+	optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+
+# compile the model
+	model.compile(loss='mean_squared_error', optimizer=optimizer)
+
+### TODO: list all unique characters in the text and remove any non-english ones
+def clean_text(text):
+    # find all unique characters in the text
 
 
-### TODO: return the text input with only ascii lowercase and the punctuation given below included.
-def cleaned_text(text):
-    punctuation = ['!', ',', '.', ':', ';', '?']
+    # remove as many non-english characters and character sequences as you can 
 
-    return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
-def window_transform_text(text, window_size, step_size):
+def window_transform_text(text,window_size,step_size):
+    # containers for input/output pairs
+
     # containers for input/output pairs
     inputs = []
     outputs = []
+    
+    size = len(text)
+    start = 0
+    while (start < size - window_size - step_size):
+        a = []
+        b = []
+        count = 0 
+        while ( count < window_size ):
+            a.append(text[start + count])
+            count = count + 1
+        step_count = 0
+        while ( step_count < step_size):
+            b.append(text[start + count + step_count])
+            step_count = step_count + 1
+        start = start + 1
+        inputs.append(a)
+        outputs.append(b)
+#        print (a)
+#        print (b)
+    # reshape each 
+#    inputs = np.asarray(inputs)
+#    inputs.shape = (np.shape(inputs)[0:2])
+#    outputs = np.asarray(outputs)
+#    outputs.shape = (len(outputs),1)
+    inputs = " ".join(str(x) for x in inputs)
+    outputs = " ".join(str(x) for x in outputs)
 
+    
     return inputs,outputs
-
-# TODO build the required RNN model: 
-# a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
-def build_part2_RNN(window_size, num_chars):
-    pass
